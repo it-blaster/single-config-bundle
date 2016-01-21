0e76cdc9aeb62cfc85524944b17071f8fe05f5e1
0x2f8f <0x2f8f@gmail.com>
2016-01-21 15:03:29 +0500

    admin

--- DependencyInjection/ItBlasterSingleConfigExtension.php
+++ DependencyInjection/ItBlasterSingleConfigExtension.php
@@ -12,7 +12,7 @@ use Symfony\Component\DependencyInjection\Loader;
  *
  * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html}
  */
-class ItBlasterConfigExtension extends Extension
+class ItBlasterSingleConfigExtension extends Extension
 {
     /**
      * {@inheritDoc}
